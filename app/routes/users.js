305e473c9899e8903255331f0de15603c72cab4b
Hawk Song <savokisong@gmail.com>
2021-03-05 22:24:33 -0800

    auth process

--- app/routes/users.js
+++ app/routes/users.js
@@ -1,4 +1,5 @@
 const Router = require('koa-router');
+const jsonwebtoken = require('jsonwebtoken');
 const router = new Router({prefix: '/users'})
 const {
     find,
@@ -9,15 +10,28 @@ const {
     login
 } = require('../controllers/users')
 
+const {secret} = require('../config');
+const auth = async (ctx, next) => {
+    const {authorization = ''} = ctx.request.header;
+    const token = authorization.replace('Bearer ', '');
+    try {
+        const user = jsonwebtoken.verify(token, secret);
+        ctx.state.user = user; //为了让后面的controller 能拿到用户信息， 放到state里面
+    } catch (err) {
+        ctx.throw(401, err.message);
+    }
+    await next();
+}
+
 router.get('/', find)
 
 router.post('/', create);
 
 router.get('/:id', findById);
 
-router.patch('/:id', update);
+router.patch('/:id', auth, update);
 
-router.delete('/:id', del);
+router.delete('/:id', auth, del);
 
 router.post('/login', login);
 
